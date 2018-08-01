91fac9da4a341fa9fc36583ddda14c56db64edd3
Julian Rozo <wdark@Julians-MacBook-Pro.local>
2018-07-31 23:25:42 -0500

    adding changes for script slider

--- profiles/andes/themes/custom/andes/js/script.js
+++ profiles/andes/themes/custom/andes/js/script.js
@@ -5,8 +5,30 @@
       slidesPerView: ((docWidth >= 1024) ? 4 : ((docWidth > 768 && docWidth <= 1024) ? 3 : ((docWidth > 640 && docWidth <= 768) ? 2 : 1)) ),
       spaceBetween: 50,
   	}
- 	//	var swiper = new Swiper("#"+jQuery('.view-componente-noticias .swiper-container-horizontal').attr('id'), data);
-	});		
+  	if ($('.view-componente-noticias .swiper-container-horizontal').length > 0) {
+  		setTimeout(function(){
+  			var swiper = new Swiper("#"+jQuery('.view-componente-noticias .swiper-container-horizontal').attr('id'), data);
+  		},50)
+  	}
+	});
+	$(window).load(function() {
+		var SlideClass = "";
+		$(".view-slider-homes .views-slideshow-controls-text-previous-processed, .view-slider-homes .views-slideshow-controls-text-next-processed").on('click', function(event) {
+			var sibling  = $(this).parents('div.views-slideshow-controls-bottom').siblings().attr('id');
+			var slideActive = $('#'+sibling).find('li.swiper-slide-active').attr('id');
+			var slideActivePosition = $('#'+slideActive+' span.field-content').children('div').first().attr('class');
+			if (SlideClass == "") {
+				$(this).parents('div.views-slideshow-controls-bottom').addClass(slideActivePosition+"-control");
+				SlideClass = slideActivePosition+"-control";
+			}
+			else{
+				$(this).parents('div.views-slideshow-controls-bottom').removeClass(SlideClass);
+				$(this).parents('div.views-slideshow-controls-bottom').addClass(slideActivePosition+"-control");
+				SlideClass = slideActivePosition+"-control";
+			}
+		});	
+	});
+		
 }(jQuery));
 
 
