14a4a4b1ff60c3fea315cb46952f6c2b3a353b77
danilosantoalvarado <danilosantosalvarado@gmail.com>
2018-08-02 15:01:59 -0500

    img movil slider

--- profiles/andes/themes/custom/andes/js/script.js
+++ profiles/andes/themes/custom/andes/js/script.js
@@ -1,13 +1,13 @@
 (function ($) {
 
-  $(window).resize(function() {
-    var docWidth = $(document).width();
-     var data = {
-      slidesPerView: ((docWidth >= 1024) ? 4 : ((docWidth > 768 && docWidth <= 1024) ? 3 : ((docWidth > 640 && docWidth <= 768) ? 2 : 1)) ),
-      spaceBetween: 50,
-    }
-   //	var swiper = new Swiper("#"+jQuery('.view-componente-noticias .swiper-container-horizontal').attr('id'), data);
-  });
+  // $(window).resize(function() {
+  //   var docWidth = $(document).width();
+  //    var data = {
+  //     slidesPerView: ((docWidth >= 1024) ? 4 : ((docWidth > 768 && docWidth <= 1024) ? 3 : ((docWidth > 640 && docWidth <= 768) ? 2 : 1)) ),
+  //     spaceBetween: 50,
+  //   }
+  //  //	var swiper = new Swiper("#"+jQuery('.view-componente-noticias .swiper-container-horizontal').attr('id'), data);
+  // });
 
   Drupal.behaviors.menuMain = {
     attach: function (context, settings) {
