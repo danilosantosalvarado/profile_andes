172c551a253f3ef3dbaf9571d8a14e5a51c227cb
bitsedwinmesa <edwin.mesa@bitsamericas.com>
2018-08-09 23:57:54 -0500

    clones funcionalidad menu mobile

--- profiles/andes/themes/custom/andes/js/script.js
+++ profiles/andes/themes/custom/andes/js/script.js
@@ -15,47 +15,65 @@
           $level1  =       $('.tb-megamenu-item.level-1'),
           $itemsNivel2  = $('.tb-megamenu-item.level-2.dropdown-submenu a');
       /* ciclo item de mega menu */
-      $(".tb-megamenu-menu-mega-menu .tb-megamenu-item.level-1").each(function (key, value) {
-        var offset         = $(this).offset(),
-            menuhover = $(this).find('.container-submenu');
-        if (menuhover.parent('li').hasClass('level-1')){
-          menuhover.css({
-            'margin-left': '-' + offset.left + 'px',
-            'width': docWidth - 10 + 'px',
-          })
-        }
-        $itemsNivel2.removeAttr('href');
-        //
-      });
-      $(".tb-megamenu-menu-mega-menu .tb-megamenu-item.level-2.dropdown-submenu ").each(function (key, value) {
-        var test = $(this).find('.tb-megamenu-subnav.level-2');
-        //.tb - megamenu - subnav.level - 2
-        console.log(test.children().length);
-        test.css({
-          'margin-top': '-'+ 32 * (key +1) +'px',
-          'position': 'absolute',
-        })
-        //console.log(this);
-        //console.log(offset);
-      });
-      $('.dropdown-toggle').on("click", function () {
-        $('.box-black').remove();
-        $(this).parents('.tb-megamenu-column').before("<div class='box-black'><a> < " + $(this).text() + "</a></div>");
-        setTimeout(function () {
-          $('.box-black').addClass('active-box').siblings('.content-img').addClass('active');
-        }, 100);
-        $(this).siblings('.nav-child').addClass('active'); /* nivel 2 animacion */
-        $(this).parents('ul.level-1').addClass('active'); /* nivel 1 animacion */
-        /* remover  */
-        $('.box-black').click(function () {
-          $('.box-black').addClass('active-box').siblings('.content-img').removeClass('eve');
-          $('.box-black').removeClass('active-box'); /* nivel 2 animacion */
-          $('.nav-child.active').removeClass('active'); /* nivel 2 animacion */
-          $('ul.level-1.active').removeClass('active'); /* nivel 1 animacion */
+      if (docWidth >1200){
+        console.log("if")
+        $(".tb-megamenu-menu-mega-menu .tb-megamenu-item.level-1").each(function (key, value) {
+          var offset         = $(this).offset(),
+              menuhover = $(this).find('.container-submenu');
+          if (menuhover.parent('li').hasClass('level-1')){
+            menuhover.css({
+              'margin-left': '-' + offset.left + 'px',
+              'width': docWidth - 10 + 'px',
+            })
+          }
+          $itemsNivel2.removeAttr('href');
+          //
         });
-      });
-    }
+        $(".tb-megamenu-menu-mega-menu .tb-megamenu-item.level-2.dropdown-submenu ").each(function (key, value) {
+          var test = $(this).find('.tb-megamenu-subnav.level-2');
+          //.tb - megamenu - subnav.level - 2
+          console.log(test.children().length);
+          test.css({
+            'margin-top': '-'+ 32 * (key +1) +'px',
+            'position': 'absolute',
+          })
+          //console.log(this);
+          //console.log(offset);
+        });
+        $('.dropdown-toggle').on("click", function () {
+          $('.box-black').remove();
+          $(this).parents('.tb-megamenu-column').before("<div class='box-black'><a> < " + $(this).text() + "</a></div>");
+          setTimeout(function () {
+            $('.box-black').addClass('active-box').siblings('.content-img').addClass('active');
+          }, 100);
+          $(this).siblings('.nav-child').addClass('active'); /* nivel 2 animacion */
+          $(this).parents('ul.level-1').addClass('active'); /* nivel 1 animacion */
+          /* remover  */
+          $('.box-black').click(function () {
+            $('.box-black').addClass('active-box').siblings('.content-img').removeClass('eve');
+            $('.box-black').removeClass('active-box'); /* nivel 2 animacion */
+            $('.nav-child.active').removeClass('active'); /* nivel 2 animacion */
+            $('ul.level-1.active').removeClass('active'); /* nivel 1 animacion */
+          });
+        });
+      }
+      else{
+        var data           = $('#block-tb-megamenu-menu-menu-top-soy ul.tb-megamenu-nav.level-0 li.level-1 a'),
+            $contentFather = $('.tb-megamenu-menu-mega-menu .always-show ul.level-0'),
+            $class = 'clone';
+        $contentFather.after("<div class='clone'>....</div>");
 
+        for (var i = 0; i < data.length; i += 1) {
+          console.log(data[i]);
+          if (data[i].title == 'Correo' ){
+            console.log(data[i])
+            console.log(data[i].title);
+            console.log(data[i].href);
+            $('.'+$class).after("<div class='clone-correo'><a href='https://www.outlook.com/uniandes.edu.co'></a>cloen</div>");
+          }
+        }
+      }
+    }
   };
 
 	//add breidpoint news component.
