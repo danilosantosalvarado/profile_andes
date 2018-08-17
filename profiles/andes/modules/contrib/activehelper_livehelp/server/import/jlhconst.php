bd5204676e06468912482b9cabf73731051ebcf4
Julian Rozo <jrozo@tbwacolombia.com>
2018-08-17 01:33:31 -0500

    adding module google cse

--- profiles/andes/modules/contrib/activehelper_livehelp/server/import/jlhconst.php
+++ profiles/andes/modules/contrib/activehelper_livehelp/server/import/jlhconst.php
@@ -1,9 +1,9 @@
 <?php
 
 define( "J_HOST", "http://localhost" );
-define( "J_DOMAIN_SET_PATH", "/Library/WebServer/Documents/uandes/profiles/andes/modules/contrib/activehelper_livehelp/server/domains" );
-define( "J_DIR_PATH", "/uandes/profiles/andes/modules/contrib/activehelper_livehelp" );
-define( "J_CONF_PATH", "/Library/WebServer/Documents/uandes/profiles/andes/modules/contrib/activehelper_livehelp" );
+define( "J_DOMAIN_SET_PATH", "/Library/WebServer/Documents/tbwa/dsit/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp/server/domains" );
+define( "J_DIR_PATH", "/tbwa/dsit/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp" );
+define( "J_CONF_PATH", "/Library/WebServer/Documents/tbwa/dsit/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp" );
 define( "J_CONF_SSL", 0 );
 
 ?>
