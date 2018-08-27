6c9d8e0b1cece5954313b4309b8966b5607c3fa9
bitsedwinmesa <edwin.mesa@bitsamericas.com>
2018-08-26 19:53:18 -0500

    menu block busqueda

--- profiles/andes/themes/custom/andes/js/script.js
+++ profiles/andes/themes/custom/andes/js/script.js
@@ -9,161 +9,7 @@
   //  //	var swiper = new Swiper("#"+jQuery('.view-componente-noticias .swiper-container-horizontal').attr('id'), data);
   // });
 
-  Drupal.behaviors.menuMain = {
-    attach: function (context, settings) {
-      var docWidth      = $(document).width(),
-          $level1  =       $('.tb-megamenu-item.level-1'),
-          $itemsNivel2  = $('.tb-megamenu-item.level-2.dropdown-submenu a');
-
-
-      $level1.hover(function () {
-        if(!this){
-          console.log("hover");
-        }
-      /*   $(this).fadeOut(100);
-        $(this).fadeIn(500); */
-      });
-
-      /* ciclo item de mega menu */
-      if (docWidth >1200){
-
-        /* Scroll menu  */
-        $(window).scroll(function (event) {
-          var scroll = $(window).scrollTop(),
-            menu = $('.tb-megamenu-menu-mega-menu');
-          if (scroll > 100) {
-            menu.removeClass('black');
-            menu.addClass('yellow');
-          }
-          else {
-            menu.removeClass('yellow');
-            menu.addClass('black');
-          }
-        });
-        /* ciclo */
-        $(".tb-megamenu-menu-mega-menu .tb-megamenu-item.level-1").each(function (key, value) {
-          var offset         = $(this).offset(),
-            menuhover = $(this).find('>.nav-child');
-          if (menuhover.parent('li').hasClass('level-1')){
-            menuhover.css({
-              'margin-left': '-' + offset.left + 'px',
-              'width': docWidth - 10 + 'px',
-            })
-          }
-          $itemsNivel2.removeAttr('href');
-          //
-        });
-        function dropdown(){
-          /* Nivel3 */
-          $('.active-clone .tb-megamenu-subnav a.dropdown-toggle').on('click', function(){
-            console.log($(this));
-            $('.box-black').parents('.row-fluid').addClass('parent-clone');
-            $('.box-black a').text('<' +$(this).text() );
-            SubMenuCloneNivel3  = $(this).siblings('.nav-child').clone().addClass('clone-nivel-3');
-            $(this).parents('.parent-clone').find('.tb-megamenu-column').after(SubMenuCloneNivel3).siblings('.active-clone').addClass('none');
-          });
-
-          $('.level-2 > .dropdown-toggle').on("click", function () {
-            $('.box-black').remove();
-            $('.active-clone').remove();
-            $(this).parents('.tb-megamenu-column').before("<div class='box-black' style='none'><a> < " + $(this).text() + "</a></div>");
-            $('.box-black').addClass('active-box').parents('.row-fluid').addClass('parent-clone');
-            setTimeout(function () {
-              $('.box-black').addClass('active-box').siblings('.content-img').addClass('active');
-              $('.box-black').addClass('active-box').siblings('.content-img').addClass('active-sub-nivel');
-            }, 100);
-            var SubMenuClone = $(this).siblings().clone().addClass('active-clone');
-
-            $(this).parents('.tb-megamenu-column').after(SubMenuClone);
-            var child = $(this).siblings('.nav-child').html();
-            var item  = $(this).parents('.level-1').find('.tb-megamenu-column-inner').html();
-            $('.box-black').click(function () {
-              $('.box-black').addClass('active-box').siblings('.content-img').removeClass('eve');
-              $('.box-black').removeClass('active-box'); /* nivel 2 animacion */
-              $('.nav-child.active').removeClass('active'); /* nivel 2 animacion */
-              $('ul.level-1.active').removeClass('active'); /* nivel 1 animacion */
-              $('.active-clone').remove();
-              $('.clone-nivel-3').remove();
-            });
-            dropdown();
-          });
-        }
-        dropdown()
-      }
-      else{
-        const
-          classSelect = 'clone-select',
-          classBlockClone = 'clone-mobile-block',
-          $contentFather = $('.tb-megamenu-menu-mega-menu .always-show ul.level-0'),
-          className = 'clone',
-          itemMobil =  $contentFather.find('.level-1  a');
-
-        itemMobil.on("click",  function () {
-          $('.box-black').remove();
-          let $this = $(this);
-          $this.siblings().addClass('active-mobile');
-          $this.siblings('.tb-megamenu-submenu').find('>.mega-dropdown-inner').before("<div class='box-black' style='none'><a> < " + $this.text() + "</a></div>");
-          console.log($this);
-          $('.box-black').click(function () {
-            console.log($this);
-            //$('.box-black').addClass('active-box').siblings('.content-img').removeClass('eve');
-            //$('.box-black').removeClass('active-box'); /* nivel 2 animacion */
-            $(this).parents('.active-mobile').removeClass('active-mobile'); /* nivel 2 animacion */
-
-          });
-        });
-
-        itemMobil.removeAttr('href');
-        $contentFather.after("<div class='" + className + "'></div>");
-        $contentFather.after("<div class='menu-soy' id='menu-soy'><span>Soy:</span><select name='' id='select-clone'><option>Elegir</option></select></div>");
-        $contentFather.after("<div class='separador-menu'></div>");
-
-        /* init */
-        generatorData(className, classBlockClone, 'block')
-        generatorData(className, classSelect, 'select')
-
-
-        /* inicia el html insertado */
-        function generatorData(className, typeClone, typeBlock){
-          var data = $('#block-tb-megamenu-menu-menu-top-soy ul.tb-megamenu-nav.level-0 li.level-1.'+ typeClone +' a');
-          loopsData(data, typeBlock);
-        }
-        /* recorre los objetos */
-        function loopsData(data, typeBlock){
-          let title = '', href = '', text = '', dataSelect = [];
-
-          for (var i = 0; i < data.length; i += 1) {
-            title = data[i].title;
-            href = data[i].href;
-            if (typeBlock == 'block'){
-              //console.log(data[i]);
-              cloneBlock(title, href);
-            }
-            if (typeBlock == 'select') {
-              text = data[i].text;
-              href = data[i].href;
-              dataSelect[i] = {'text':text,'href':href};
-            }
-          }//endFor
-          cloneSelect(dataSelect);
-        }
-        /* funcion Clone de bloques */
-        function cloneBlock(title, href){
-          let clone = $('.' + className).append(
-            "<div class='" + className + "-block " + className + "-" + title.toLowerCase() + "'><a href='" + href + "'>" + title + "</a></div>");
-          return clone;
-        }
-        /* clone select */
-        function cloneSelect(obj) {
-          for (i = 0; i < obj.length; i++) {
-            let $htmloption = $('#select-clone').append('<option class="' + i + ' "value="' + obj[i].text + '" data-href="' + obj[i].href + '">' + obj[i].text + '</option>');
-            //console.log(htmloption)
-          }
-          //return $htmloption;
-        }
-      }
-    }
-  };
+  ;
 		var SlideClass = "";
     var ParentSlideClass = "";
 	//add breidpoint news component.
@@ -280,14 +126,14 @@ setTimeout(function(){
 		// });
 
   });
-  
+
   // funcion abir barra de busqueda
   $(document).ready(function() {
-    $(".tb-megamenu-menu-mega-menu .nav-collapse").append('<div class="barra-buscar"><span>Cerrar</span></div>');
+    /* $(".wrapper-mega-menu").append('<div class="barra-buscar mega-menu-2"><span>Cerrar</span></div>');
 
     $(".barra-buscar").click(function(){
       $( '#block-google-cse-google-cse' ).toggleClass( "buscador-open" );
-    });
+    }); */
   });
 
 }(jQuery));
