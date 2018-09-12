531106f524a001dcdddf0a9fed7c9e5dc30fc2ca
Julian Rozo <jrozo@tbwacolombia.com>
2018-09-12 17:31:01 -0500

    adding changes for anclas

--- profiles/andes/modules/custom/menu_lateral/js/anclas.js
+++ profiles/andes/modules/custom/menu_lateral/js/anclas.js
@@ -4,31 +4,35 @@
 		var atributo = [];
 		$(".menu-lateral").each(function(){
 			var html = $(this).attr('id');
-			var html = html.replace("-"," ");			
+			var html = html.replace("-"," ");
 
 			atributo.push('<li class="seccions"><a href="#'+$(this).attr('id')+'"><div><p class="seccion-text">'+html+'</p></div></a></li>');
-  		   
+
 		});
 
 		for (var i = atributo.length - 1; i >= 0; i--) {
 			$("#render-menu-lateral").prepend(atributo[i]);
 		}
-		
+
 		$("#render-menu-lateral").append('<li class="l-chat"> <a href=""> <div> <p class="seccions"> Chat </p> </div></a></li>');
 
 
 		// on-click
 
 		$(function(){
-		     $('a[href*=#]').click(function() {
+		     $('a[href*=#]').click(function(e) {
+				 e.preventDefault();
 				$("#render-menu-lateral li").removeClass("a-active");
 				$(this).parent().addClass('a-active')
 		     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
 		         && location.hostname == this.hostname) {
-		             var $target = $(this.hash);
+					 var $target = $(this.hash);
+					 var $menu_admin = ($('#admin-menu').length > 0) ? $('#admin-menu').height() : 0 ;
+					 var $menu_navigation = ($('.region-navigation').length > 0  ) ? $('.region-navigation').height() : 0 ;
 		         	$target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
 		             if ($target.length) {
-		                 var targetOffset = $target.offset().top;
+
+		                 var targetOffset = $target.offset().top-$menu_admin -  $menu_navigation;
 		                 $('html,body').animate({scrollTop: targetOffset}, 2000);
 		                 return false;
 		            }
