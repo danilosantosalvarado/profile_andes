5e1575c8aa919a8613dfec750ec5f7975aa589f3
jdcb1218 <jdcb1218@gmail.com>
2018-12-06 10:54:08 -0500

    Codec Standars.

--- profiles/andes/modules/contrib/activehelper_livehelp/server/import/jlhconst.php
+++ profiles/andes/modules/contrib/activehelper_livehelp/server/import/jlhconst.php
@@ -1,9 +1,9 @@
 <?php
 
 define( "J_HOST", "http://localhost" );
-define( "J_DOMAIN_SET_PATH", "/Library/WebServer/Documents/tbwa/dsit/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp/server/domains" );
-define( "J_DIR_PATH", "/tbwa/dsit/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp" );
-define( "J_CONF_PATH", "/Library/WebServer/Documents/tbwa/dsit/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp" );
+define( "J_DOMAIN_SET_PATH", "/Library/WebServer/Documents/profile/profiles/andes/modules/contrib/activehelper_livehelp/server/domains" );
+define( "J_DIR_PATH", "/profile/profiles/andes/modules/contrib/activehelper_livehelp" );
+define( "J_CONF_PATH", "/Library/WebServer/Documents/profile/profiles/andes/modules/contrib/activehelper_livehelp" );
 define( "J_CONF_SSL", 0 );
 
 ?>
