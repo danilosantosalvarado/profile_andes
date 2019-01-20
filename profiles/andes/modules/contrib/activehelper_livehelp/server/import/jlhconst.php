88d39b34550d6628336abc224c387d4a04034e3e
Julian Rozo <jrozo@tbwacolombia.com>
2019-01-20 00:06:44 -0500

    adding changes for profile finish

--- profiles/andes/modules/contrib/activehelper_livehelp/server/import/jlhconst.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define( "J_HOST", "http://localhost" );
-define( "J_DOMAIN_SET_PATH", "/Library/WebServer/Documents/profile/profiles/andes/modules/contrib/activehelper_livehelp/server/domains" );
-define( "J_DIR_PATH", "/profile/profiles/andes/modules/contrib/activehelper_livehelp" );
-define( "J_CONF_PATH", "/Library/WebServer/Documents/profile/profiles/andes/modules/contrib/activehelper_livehelp" );
-define( "J_CONF_SSL", 0 );
-
-?>
