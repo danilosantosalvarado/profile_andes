40d3191c19cf562326c30a4e139ca1133c073e2e
tbwadevelop <tbwadevelop@gmail.com>
2019-06-14 18:26:51 -0500

    bootstrap y adminimal modules de varnish y memcache

---
+++ profiles/andes/modules/contrib/activehelper_livehelp/server/import/jlhconst.php
@@ -0,0 +1,9 @@
+<?php
+
+define( "J_HOST", "https://tbwacolombia.com" );
+define( "J_DOMAIN_SET_PATH", "/var/www/vhosts/tbwacolombia.com/httpdocs/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp/server/domains" );
+define( "J_DIR_PATH", "/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp" );
+define( "J_CONF_PATH", "/var/www/vhosts/tbwacolombia.com/httpdocs/plantilla_base/profiles/andes/modules/contrib/activehelper_livehelp" );
+define( "J_CONF_SSL", 1 );
+
+?>
--- profiles/andes/themes/contrib/bootstrap/theme-settings.php
+++ profiles/andes/themes/contrib/bootstrap/theme-settings.php
@@ -5,6 +5,8 @@
  * theme-settings.php
  *
  * Provides theme settings for Bootstrap based themes when admin theme is not.
+ *
+ * @see ./includes/settings.inc
  */
 
 /**
